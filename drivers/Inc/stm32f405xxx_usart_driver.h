4f4993fd8b7bb231839a02dfef24ef3fd9da1c24
Ashwin-x306 <ashwinsenthil2608@gmail.com>
2026-08-10 02:28:33 +0530

    Update stm32f405xxx_usart_driver.h

--- drivers/Inc/stm32f405xxx_usart_driver.h
+++ drivers/Inc/stm32f405xxx_usart_driver.h
@@ -1,4 +1,14 @@
+/*
+ * stm32f405xxx_usart_driver.h
+ *
+ *  Created on: Aug 10, 2026
+ *      Author: Ashwin
+ */
 
+#ifndef INC_STM32F405XXX_USART_DRIVER_H_
+#define INC_STM32F405XXX_USART_DRIVER_H_
+
+#include"stm32f405xxx.h"
 
 /*
  * Configuration structure for USARTx peripheral
@@ -18,24 +28,30 @@ typedef struct
 {
 	USART_RegDef_t *pUSARTx;
 	USART_Config_t   USART_Config;
+	uint8_t          *pTxBuffer;   /* app Tx buffer address */
+	uint8_t          *pRxBuffer;   /* app Rx buffer address */
+	uint32_t         TxLen;        /* Tx length */
+	uint32_t         RxLen;        /* Rx length */
+	uint8_t          TxBusyState;  /* communication state */
+	uint8_t          RxBusyState;
 }USART_Handle_t;
-
 
 
 /*
  *@USART_Mode
- *Possible options for USART_Mode
  */
 #define USART_MODE_ONLY_TX 0
 #define USART_MODE_ONLY_RX 1
 #define USART_MODE_TXRX  2
+#define NO_PR_BITS_IMPLEMENTED   4
 
+#define SET     ENABLE
+#define RESET   DISABLE
 /*
  *@USART_Baud
- *Possible options for USART_Baud
  */
 #define USART_STD_BAUD_1200					1200
-#define USART_STD_BAUD_2400					400
+#define USART_STD_BAUD_2400					2400
 #define USART_STD_BAUD_9600					9600
 #define USART_STD_BAUD_19200 				19200
 #define USART_STD_BAUD_38400 				38400
@@ -45,12 +61,10 @@ typedef struct
 #define USART_STD_BAUD_460800 				460800
 #define USART_STD_BAUD_921600 				921600
 #define USART_STD_BAUD_2M 					2000000
-#define SUART_STD_BAUD_3M 					3000000
-
+#define USART_STD_BAUD_3M 					3000000
 
 /*
  *@USART_ParityControl
- *Possible options for USART_ParityControl
  */
 #define USART_PARITY_EN_ODD   2
 #define USART_PARITY_EN_EVEN  1
@@ -58,14 +72,12 @@ typedef struct
 
 /*
  *@USART_WordLength
- *Possible options for USART_WordLength
  */
 #define USART_WORDLEN_8BITS  0
 #define USART_WORDLEN_9BITS  1
 
 /*
  *@USART_NoOfStopBits
- *Possible options for USART_NoOfStopBits
  */
 #define USART_STOPBITS_1     0
 #define USART_STOPBITS_0_5   1
@@ -74,19 +86,104 @@ typedef struct
 
 /*
  *@USART_HWFlowControl
- *Possible options for USART_HWFlowControl
  */
 #define USART_HW_FLOW_CTRL_NONE    	0
 #define USART_HW_FLOW_CTRL_CTS    	1
 #define USART_HW_FLOW_CTRL_RTS    	2
 #define USART_HW_FLOW_CTRL_CTS_RTS	3
 
+/*
+ * USART application states
+ */
+#define USART_READY			0
+#define USART_BUSY_IN_RX		1
+#define USART_BUSY_IN_TX		2
+
+/*
+ * USART application events
+ */
+#define USART_EVENT_TX_CMPLT   0
+#define USART_EVENT_RX_CMPLT   1
+#define USART_EVENT_IDLE       2
+#define USART_EVENT_CTS        3
+#define USART_EVENT_PE         4
+#define USART_ERR_FE     	    5
+#define USART_ERR_NE    	 	6
+#define USART_ERR_ORE    	 	7
+
+/*
+ * Bit position definitions of USART peripheral registers
+ * (CR1)
+ */
+#define USART_CR1_SBK			0
+#define USART_CR1_RWU 			1
+#define USART_CR1_RE 			2
+#define USART_CR1_TE 			3
+#define USART_CR1_IDLEIE 		4
+#define USART_CR1_RXNEIE 		5
+#define USART_CR1_TCIE 			6
+#define USART_CR1_TXEIE 		7
+#define USART_CR1_PEIE 			8
+#define USART_CR1_PS 			9
+#define USART_CR1_PCE 			10
+#define USART_CR1_WAKE 			11
+#define USART_CR1_M 			12
+#define USART_CR1_UE 			13
+#define USART_CR1_OVER8 		15
+
+/*
+ * Bit position definitions (CR2)
+ */
+#define USART_CR2_ADD   		0
+#define USART_CR2_LBDL   		5
+#define USART_CR2_LBDIE  		6
+#define USART_CR2_LBCL   		8
+#define USART_CR2_CPHA   		9
+#define USART_CR2_CPOL   		10
+#define USART_CR2_CLKEN  		11
+#define USART_CR2_STOP   		12
+#define USART_CR2_LINEN  		14
+
+/*
+ * Bit position definitions (CR3)
+ */
+#define USART_CR3_EIE   		0
+#define USART_CR3_IREN   		1
+#define USART_CR3_IRLP  		2
+#define USART_CR3_HDSEL   		3
+#define USART_CR3_NACK   		4
+#define USART_CR3_SCEN   		5
+#define USART_CR3_DMAR  		6
+#define USART_CR3_DMAT   		7
+#define USART_CR3_RTSE   		8
+#define USART_CR3_CTSE   		9
+#define USART_CR3_CTSIE   		10
+#define USART_CR3_ONEBIT   		11
+
+/*
+ * Bit position definitions (SR)
+ */
+#define USART_SR_PE       		0
+#define USART_SR_FE       		1
+#define USART_SR_NE				2
+#define USART_SR_ORE      		3
+#define USART_SR_IDLE     		4
+#define USART_SR_RXNE          5
+#define USART_SR_TC				6
+#define USART_SR_TXE			7
+#define USART_SR_LBD			8
+#define USART_SR_CTS			9
+
+/*
+ *@USART_FlagName  (to be used with USART_GetFlagStatus / USART_ClearFlag)
+ */
+#define USART_FLAG_TXE 		(1 << USART_SR_TXE)
+#define USART_FLAG_RXNE 		(1 << USART_SR_RXNE)
+#define USART_FLAG_TC 			(1 << USART_SR_TC)
+#define USART_FLAG_IDLE		(1 << USART_SR_IDLE)
+#define USART_FLAG_ORE			(1 << USART_SR_ORE)
 
 
-/******************************************************************************************
- *								APIs supported by this driver
- *		 For more information about the APIs check the function definitions
- ******************************************************************************************/
 /*
  * Peripheral Clock setup
  */
@@ -98,6 +195,10 @@ void USART_PeriClockControl(USART_RegDef_t *pUSARTx, uint8_t EnorDi);
 void USART_Init(USART_Handle_t *pUSARTHandle);
 void USART_DeInit(USART_RegDef_t *pUSARTx);
 
+/*
+ * Baud rate
+ */
+void USART_SetBaudRate(USART_RegDef_t *pUSARTx, uint32_t BaudRate);
 
 /*
  * Data Send and Receive
@@ -125,3 +226,6 @@ void USART_ClearFlag(USART_RegDef_t *pUSARTx, uint16_t StatusFlagName);
  * Application callback
  */
 void USART_ApplicationEventCallback(USART_Handle_t *pUSARTHandle,uint8_t AppEv);
+uint32_t RCC_GetPCLK1Value(void);
+uint32_t RCC_GetPCLK2Value(void);
+#endif /* INC_STM32F405XXX_USART_DRIVER_H_ */
